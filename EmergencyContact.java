c598b5ba3d032129d6b05c7195ccd2438ded4a34
Cholmes24 <32798433+Cholmes24@users.noreply.github.com>
2017-11-25 03:36:35 -0500

    Add files via upload

---
+++ EmergencyContact.java
@@ -0,0 +1,144 @@
+package Constructs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class EmergencyContact {
+	private String name;
+	private String address;
+	private String phone;
+	private int ID;
+	private int EID;
+	private String sqlCommand;
+	public EmergencyContact() {
+	}
+	public EmergencyContact(int ID) {
+		this.ID=ID;
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			ResultSet reslt = stmnt.executeQuery("SELECT * FROM Emergency_Contact WHERE Emergency_Contact_ID="+ID+";");
+			if(reslt.next()) {
+				this.name=reslt.getString("Name");
+				this.address=reslt.getString("Address");
+				this.phone=reslt.getString("Phone_Number");
+				this.EID=reslt.getInt("employee_Employee_ID");
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	public EmergencyContact(String name, String address, String num, int EID ) {
+		this.name=name;
+		this.address=address;
+		this.phone=num;
+		this.EID=EID;
+		sqlCommand="INSERT INTO Emergency_Contact (Name,Address,Phone_Number,employee_Employee_ID) values(\""+name+"\",\""+address+"\",\""+num+"\","+EID+");";
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			stmnt.executeUpdate(sqlCommand);
+			ResultSet reslt = stmnt.executeQuery("SELECT Emergency_Contact_ID FROM Emergency_Contact WHERE Phone_Number=\""+phone+"\";");
+			if(reslt.next()) {
+				this.ID=reslt.getInt("Emergency_Contact_ID");
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	public String getRecentContact() {
+		return "Name: "+name+"\nADDRESS: "+address+"\nPhone Number: "+phone;
+	}
+	public String getPrimaryContact() {
+		String phone=null;
+		String name=null;
+		String address=null;
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			ResultSet reslt = stmnt.executeQuery("SELECT * FROM Emergency_Contact WHERE Emergency_Contact_ID="+1+";");
+			if(reslt.next()) {
+				phone=reslt.getString("Phone_Number");
+				name=reslt.getString("Name");
+				address=reslt.getString("Address");
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		if(phone.equals(null)||name.equals(null)||address.equals(null)) {
+			return"Emergency Contact Incomplete";
+		}else {
+			return "Name: "+name+"\nADDRESS: "+address+"\nPHONE NUMBER: "+phone;
+		}
+	}
+	public EmergencyContact [] getAllContacts() {
+		Connection conn=null;
+		EmergencyContact [] a = null;
+		int i=0;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			ResultSet reslt = stmnt.executeQuery("SELECT MAX(Emergency_Contact_ID) FROM Emergency_Contact;");
+			if(reslt.next()) 
+				a= new EmergencyContact [reslt.getInt("MAX(Emergency_Contact_ID)")];
+			reslt = stmnt.executeQuery("SELECT * FROM Emergency_Contact ;");
+			while(reslt.next()) {
+				a[i]= new EmergencyContact(reslt.getInt("Emergency_Contact_ID"));
+				i++;
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+			return a;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public int getID() {
+		return ID;
+	}
+	public int getEID() {
+		return EID;
+	}
+	public void setName(String name) {
+		this.name=name;
+		this.sqlCommand="UPDATE Emergency_Contact SET Name=\""+name+"\" WHERE Emergency_Contact_ID="+ID+";";
+		SQLInterface();
+	}
+	public void setAddress(String address) {
+		this.address=address;
+		this.sqlCommand="UPDATE Emergency_Contact SET Address=\""+address+"\" WHERE Emergency_Contact_ID="+ID+";";
+		SQLInterface();
+	}
+	public void setPhone(String phone) {
+		this.phone=phone;
+		this.sqlCommand="UPDATE Emergency_Contact SET Phone_Number=\""+phone+"\" WHERE Emergency_Contact_ID="+ID+";";
+		SQLInterface();
+	}
+	public void SQLInterface() {
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			stmnt.executeUpdate(sqlCommand);
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
---
+++ Employee.java
@@ -0,0 +1,192 @@
+package Constructs;
+import java.sql.*;
+public class Employee {
+	private int ID;
+	private String firstName;
+	private String lastName;
+	private String title;
+	private int ssn;
+	private String address;
+	private String phone;
+	private EmergencyContact a;
+	private EmployeeShifts b;
+	private String sqlCommand;
+	public Employee() {
+	}
+	public Employee(int ID) {
+		this.ID=ID;
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			ResultSet reslt = stmnt.executeQuery("SELECT * FROM Employee WHERE EMPLOYEE_ID="+ID+";");
+			if(reslt.next()) {
+				this.lastName=reslt.getString("Last_Name");
+				this.firstName=reslt.getString("First_Name");
+				this.title=reslt.getString("Job_Title");
+				this.ssn=reslt.getInt("SSN");
+				this.address=reslt.getString("Address");
+				this.phone=reslt.getString("Phone_Number");
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	public Employee(String lastName,String firstName,String title,int ssn) {
+		this.lastName=lastName;
+		this.firstName=firstName;
+		this.title=title;
+		this.ssn=ssn;
+		sqlCommand="INSERT INTO Employee (Last_Name,First_Name,Job_Title,SSN) values(\""+lastName+"\",\""+firstName+"\",\""+title+"\","+ssn+");";
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			stmnt.executeUpdate(sqlCommand);
+			ResultSet reslt = stmnt.executeQuery("SELECT Employee_ID FROM Employee WHERE SSN="+ssn+";");
+			if(reslt.next()) {
+				this.ID=reslt.getInt("Employee_ID");
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	public Employee(String lastName,String firstName,String title,int ssn, String address, String phone) {
+		this.lastName=lastName;
+		this.firstName=firstName;
+		this.title=title;
+		this.ssn=ssn;
+		this.address=address;
+		this.phone=phone;
+		sqlCommand="INSERT INTO Employee (Last_Name,First_Name,Job_Title,SSN,Address,Phone_Number) values(\""+lastName+"\",\""+firstName+"\",\""+title+"\","+ssn+",\""+address+"\",\""+phone+"\");";
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			stmnt.executeUpdate(sqlCommand);
+			ResultSet reslt = stmnt.executeQuery("SELECT Employee_ID FROM Employee WHERE SSN="+ssn+";");
+			if(reslt.next()) {
+				this.ID=reslt.getInt("Employee_ID");
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	public int getID() {
+		return ID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public int getSSN() {
+		return ssn;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public String getContact() {
+		return "Name: "+firstName+" "+lastName+"\nPHONE NUMBER: "+phone+"\nADDRESS: "+address;
+	}
+	public String getFullTitle() {
+		return firstName+" "+lastName+" is a:"+title;
+	}
+	public Employee [] getAllEID() {
+		Connection conn=null;
+		Employee [] a=null;
+		int i=0;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			ResultSet reslt = stmnt.executeQuery("SELECT MAX(Employee_ID) FROM EMPLOYEE;");
+			if(reslt.next()) {
+				a=new Employee [reslt.getInt("MAX(Employee_ID)")];
+				reslt = stmnt.executeQuery("SELECT Employee_ID FROM EMPLOYEE;");
+				while(reslt.next()) {
+					a [i]=new Employee (reslt.getInt("Employee_ID"));
+					i++;
+				}
+			}
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return a;
+	}
+	public void setLastName(String name) {
+		this.lastName=name;
+		this.sqlCommand="UPDATE Employee SET Last_Name=\""+lastName+"\" WHERE Employee_ID="+ID+";";
+		SQLInterface();
+		if(b!=null)
+		b.setLastName(name);
+	}
+	public void setFirstName(String name) {
+		this.firstName=name;
+		this.sqlCommand="UPDATE Employee SET First_Name=\""+firstName+"\" WHERE Employee_ID="+ID+";";
+		SQLInterface();
+		if(b!=null)
+		b.setFirstName(name);
+	}
+	public void setJob_Title(String t) {
+		this.title=t;
+		this.sqlCommand="UPDATE Employee SET Job_Title=\""+title+"\" WHERE Employee_ID="+ID+";";
+		SQLInterface();
+	}
+	public void setSSN(int n) {
+		this.ssn=n;
+		this.sqlCommand="UPDATE Employee SET SSN="+ssn+" WHERE Employee_ID="+ID+";";
+		SQLInterface();
+	}
+	public void setAddress(String a) {
+		this.address=a;
+		this.sqlCommand="UPDATE Employee SET Address=\""+address+"\" WHERE Employee_ID="+ID+";";
+		SQLInterface();
+	}
+	public void setPhone(String n) {
+		this.phone=n;
+		this.sqlCommand="UPDATE Employee SET Phone_Number=\""+phone+"\" WHERE Employee_ID="+ID+";";
+		SQLInterface();
+	}
+	public void createEmergencyContact(String name,String address,String num) {
+		a=new EmergencyContact(name,address,num,ID);
+	}
+	public EmergencyContact getEmergencyContact() {
+		return a;
+	}
+	public EmergencyContact accessEmergencyContact(int contactID) {
+		a=new EmergencyContact();
+		return a;
+	}
+	public void createShift(String shift,String clockIn, String clockOut) {
+		b=new EmployeeShifts(shift,clockIn,clockOut,ID,lastName,firstName);
+	}
+	public EmployeeShifts getShifts() {
+		return b;
+	}
+	public EmployeeShifts accessShifts(int day) {
+		b=new EmployeeShifts();
+		return b;
+	}
+	public void SQLInterface() {
+		Connection conn=null;
+		try {
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/Manager?useSSL=false","student","student");
+			Statement stmnt = conn.createStatement();
+			stmnt.executeUpdate(sqlCommand);
+			conn.close();
+		}catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
